c0e8ba44a46ce16ee67b81d5a4600581f6227ba7
Krzysztof Wencel <kwencel@users.noreply.github.com>
2021-10-13 21:45:21 +0200

    Bootstrap the project via Spring Initializr

---
+++ src/main/kotlin/io/github/kwencel/backendshowcase/BackendShowcaseApplication.kt
@@ -0,0 +1,11 @@
+package io.github.kwencel.backendshowcase
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendShowcaseApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendShowcaseApplication>(*args)
+}
